7da7d79e55fd171ae883d02c30733743f992305d
Lewis Cowles <lewiscowles@me.com>
2015-06-20 00:44:18 +0100

    Create category_pages.rb
    Improved example provided at http://jekyllrb.com/docs/plugins/

---
+++ _plugins/category_pages.rb
@@ -0,0 +1,42 @@
+module Jekyll
+
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+
+      self.data['iscat'] = true
+
+      if category != 'index.html'
+        self.data['category'] = category
+        category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+        self.data['title'] = "#{category_title_prefix}#{category}"
+      else
+        self.data['title'] = 'Categories:'
+      end
+
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      dir = site.config['category_dir'] || 'categories'
+      if site.layouts.key? 'category_index'
+        site.categories.each_key do |category|
+          newpage = CategoryPage.new(site, site.source, File.join(dir, category), category)
+          site.pages << newpage
+          #categories << newpage.title
+        end
+      end
+      site.pages << CategoryPage.new(site, site.source, dir, "index.html")
+    end
+  end
+
+end
